0bbed532034f64fe7c0db19a038e0fb9454eb871
claudiodamico <64013841+claudiodamico@users.noreply.github.com>
2020-06-09 09:04:46 -0300

    Inicio

---
+++ TP1/TP1/ArbolGeneral.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP1
+{
+	public class ArbolGeneral<T>
+	{
+		
+		private NodoGeneral<T> raiz;
+
+		public ArbolGeneral(T dato) {
+			this.raiz = new NodoGeneral<T>(dato);
+		}
+	
+		private ArbolGeneral(NodoGeneral<T> nodo) {
+			this.raiz = nodo;
+		}
+	
+		private NodoGeneral<T> getRaiz() {
+			return raiz;
+		}
+	
+		public T getDatoRaiz() {
+			return this.getRaiz().getDato();
+		}
+	
+		public List<ArbolGeneral<T>> getHijos() {
+			List<ArbolGeneral<T>> temp= new List<ArbolGeneral<T>>();
+			foreach (var element in this.raiz.getHijos()) {
+				temp.Add(new ArbolGeneral<T>(element));
+			}
+			return temp;
+		}
+	
+		public void agregarHijo(ArbolGeneral<T> hijo) {
+			this.raiz.getHijos().Add(hijo.getRaiz());
+		}
+	
+		public void eliminarHijo(ArbolGeneral<T> hijo) {
+			this.raiz.getHijos().Remove(hijo.getRaiz());
+		}
+	
+		public bool esVacio() {
+			return this.raiz == null;
+		}
+	
+		public bool esHoja() {
+			return this.raiz != null && this.getHijos().Count == 0;
+		}
+	
+		public int altura() {
+			return 0;
+		}
+	
+		
+		public int nivel(T dato) {
+			return 0;
+		}
+
+		public int Altura()
+		{
+			if (this.esVacio())
+			{
+				return -1;
+			}
+			if (this.esHoja())
+			{
+				return 0;
+			}
+			else
+			{
+				int alturaMaxima = 0;
+				foreach (var hijo in this.getHijos())
+				{
+					int alturaHijo = hijo.Altura();
+					if (alturaMaxima < alturaHijo)
+					{
+						alturaMaxima = alturaHijo;
+					}
+				}
+				return alturaMaxima + 1;
+			}
+		}
+
+
+	}
+
+	
+
+
+}
---
+++ TP1/TP1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolGeneral<int> arbol = new ArbolGeneral<int>(10);
+
+            ArbolGeneral<int> hijo1 = new ArbolGeneral<int>(20);
+            ArbolGeneral<int> hijo2 = new ArbolGeneral<int>(30);
+            ArbolGeneral<int> hijo3 = new ArbolGeneral<int>(40);
+
+            arbol.agregarHijo(hijo1);
+            arbol.agregarHijo(hijo2);
+            arbol.agregarHijo(hijo3);
+
+            for (int i = 1; i < 3; i++)
+            {
+                hijo1.agregarHijo (new ArbolGeneral<int>(i + 20));
+                hijo2.agregarHijo (new ArbolGeneral<int>(i + 30));
+                hijo3.agregarHijo (new ArbolGeneral<int>(i + 40));
+
+            }
+
+            Console.WriteLine(arbol.Altura());
+            arbol.Altura();
+            Console.ReadKey(true);
+            
+        }	
+
+	}
+
+    
+	
+
+}
