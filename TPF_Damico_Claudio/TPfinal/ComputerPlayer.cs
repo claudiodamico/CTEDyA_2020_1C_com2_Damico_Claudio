62018838555e1bab84de655243de4c2028485aa8
claudiodamico <64013841+claudiodamico@users.noreply.github.com>
2020-06-29 15:07:28 -0300

    Constructor agregado

--- TPF_Damico_Claudio/TPfinal/ComputerPlayer.cs
+++ TPF_Damico_Claudio/TPfinal/ComputerPlayer.cs
@@ -6,9 +6,9 @@ using System.Linq;
 namespace juegoIA
 {
 	public class ComputerPlayer: Jugador
-	{ 
+	{       //lista de naipes
 		private List<int> naipes = new List<int>();
-
+		    //lista arbol
 		private ArbolGeneral<jugada> arbol;
 
 
@@ -33,13 +33,22 @@ namespace juegoIA
 			//implementar
 			
 		}
-
+		   //clase nueva
 		public class jugada
         {
 			int carta;
 			int limiteActual;
 			int ganadas;
 			bool esAI;
+
+		   //constructor de la clase jugada
+		   public jugada(int carta, int limiteActual, int ganadas, bool esAI)
+            {
+				this.carta = carta;
+				this.limiteActual = limiteActual;
+				this.ganadas = ganadas;
+				this.esAI = esAI;
+            }
         }
 		
 	}
